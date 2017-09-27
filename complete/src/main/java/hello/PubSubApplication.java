51ce209df6db9dcabc169555c2da14b2215f3a6f
João Martins <joaoandremartins@users.noreply.github.com>
2017-09-27 11:14:33 -0400

    Adapts getting started guide to use most recent upstream changes (#3)
    There were some changes on the Spring Integration channel adapters
    for GCP Pub/Sub that would make the example not compile.

--- complete/src/main/java/hello/PubSubApplication.java
+++ complete/src/main/java/hello/PubSubApplication.java
@@ -1,23 +1,24 @@
 package hello;
 
-import com.google.cloud.pubsub.v1.AckReplyConsumer;
-import com.google.protobuf.ByteString;
 import java.io.IOException;
+
+import com.google.cloud.pubsub.v1.AckReplyConsumer;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gcp.pubsub.core.PubSubOperations;
 import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
 import org.springframework.cloud.gcp.pubsub.support.GcpHeaders;
-import org.springframework.cloud.gcp.pubsub.support.SubscriberFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
-import org.springframework.integration.gcp.AckMode;
-import org.springframework.integration.gcp.inbound.PubSubInboundChannelAdapter;
-import org.springframework.integration.gcp.outbound.PubSubMessageHandler;
+import org.springframework.integration.gcp.pubsub.AckMode;
+import org.springframework.integration.gcp.pubsub.inbound.PubSubInboundChannelAdapter;
+import org.springframework.integration.gcp.pubsub.outbound.PubSubMessageHandler;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
 
@@ -43,9 +44,9 @@ public class PubSubApplication {
   @Bean
   public PubSubInboundChannelAdapter messageChannelAdapter(
       @Qualifier("pubsubInputChannel") MessageChannel inputChannel,
-      SubscriberFactory subscriberFactory) {
+      PubSubOperations pubSubTemplate) {
     PubSubInboundChannelAdapter adapter =
-        new PubSubInboundChannelAdapter(subscriberFactory, "testSubscription");
+        new PubSubInboundChannelAdapter(pubSubTemplate, "testSubscription");
     adapter.setOutputChannel(inputChannel);
     adapter.setAckMode(AckMode.MANUAL);
 
@@ -58,8 +59,7 @@ public class PubSubApplication {
   @ServiceActivator(inputChannel = "pubsubInputChannel")
   public MessageHandler messageReceiver() {
     return message -> {
-      LOGGER.info("Message arrived! Payload: "
-          + ((ByteString) message.getPayload()).toStringUtf8());
+      LOGGER.info("Message arrived! Payload: " + message.getPayload());
       AckReplyConsumer consumer =
           (AckReplyConsumer) message.getHeaders().get(GcpHeaders.ACKNOWLEDGEMENT);
       consumer.ack();
@@ -72,10 +72,8 @@ public class PubSubApplication {
   // tag::messageSender[]
   @Bean
   @ServiceActivator(inputChannel = "pubsubOutputChannel")
-  public MessageHandler messageSender(PubSubTemplate pubsubTemplate) {
-    PubSubMessageHandler outboundAdapter = new PubSubMessageHandler(pubsubTemplate);
-    outboundAdapter.setTopic("testTopic");
-    return outboundAdapter;
+  public MessageHandler messageSender(PubSubOperations pubsubTemplate) {
+    return new PubSubMessageHandler(pubsubTemplate, "testTopic");
   }
   // end::messageSender[]
 
