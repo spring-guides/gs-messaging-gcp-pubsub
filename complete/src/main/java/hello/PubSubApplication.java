328941c0faa20fbe4a98791c31f7c2d3823e808a
João André Martins <joaomartins@google.com>
2018-02-06 11:01:40 -0500

    Updates guide to use spring-cloud-gcp M2

--- complete/src/main/java/hello/PubSubApplication.java
+++ complete/src/main/java/hello/PubSubApplication.java
@@ -10,15 +10,14 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gcp.pubsub.core.PubSubOperations;
-import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
+import org.springframework.cloud.gcp.pubsub.integration.AckMode;
+import org.springframework.cloud.gcp.pubsub.integration.inbound.PubSubInboundChannelAdapter;
+import org.springframework.cloud.gcp.pubsub.integration.outbound.PubSubMessageHandler;
 import org.springframework.cloud.gcp.pubsub.support.GcpHeaders;
 import org.springframework.context.annotation.Bean;
 import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
-import org.springframework.integration.gcp.pubsub.AckMode;
-import org.springframework.integration.gcp.pubsub.inbound.PubSubInboundChannelAdapter;
-import org.springframework.integration.gcp.pubsub.outbound.PubSubMessageHandler;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
 
