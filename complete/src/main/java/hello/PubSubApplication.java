e01fc1ee8a10e27f36979eeeb0bf3642f334ff03
João Martins <joaoandremartins@users.noreply.github.com>
2018-04-13 11:27:19 -0400

    Updates to spring-cloud-gcp M3
    * Polish
    
    * Remove SI version entirely
    
    * Updates from M2 to M3

--- complete/src/main/java/hello/PubSubApplication.java
+++ complete/src/main/java/hello/PubSubApplication.java
@@ -2,10 +2,8 @@ package hello;
 
 import java.io.IOException;
 
-import com.google.cloud.pubsub.v1.AckReplyConsumer;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,13 +11,15 @@ import org.springframework.cloud.gcp.pubsub.core.PubSubOperations;
 import org.springframework.cloud.gcp.pubsub.integration.AckMode;
 import org.springframework.cloud.gcp.pubsub.integration.inbound.PubSubInboundChannelAdapter;
 import org.springframework.cloud.gcp.pubsub.integration.outbound.PubSubMessageHandler;
-import org.springframework.cloud.gcp.pubsub.support.GcpHeaders;
+import org.springframework.cloud.gcp.pubsub.support.GcpPubSubHeaders;
 import org.springframework.context.annotation.Bean;
 import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
+
+import com.google.cloud.pubsub.v1.AckReplyConsumer;
 
 @SpringBootApplication
 public class PubSubApplication {
@@ -60,7 +60,7 @@ public class PubSubApplication {
     return message -> {
       LOGGER.info("Message arrived! Payload: " + message.getPayload());
       AckReplyConsumer consumer =
-          (AckReplyConsumer) message.getHeaders().get(GcpHeaders.ACKNOWLEDGEMENT);
+          (AckReplyConsumer) message.getHeaders().get(GcpPubSubHeaders.ACKNOWLEDGEMENT);
       consumer.ack();
     };
   }
