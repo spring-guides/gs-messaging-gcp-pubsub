b1bdcc5fb64b1eece4b1f63d5d2a5ce07cf6a3a4
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2026-02-06 19:14:09 +0100

    Add empty application in initial/
    Closes gh-36

---
+++ initial/src/main/java/hello/PubSubApplication.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubSubApplication {
+
+  public static void main(String[] args) {
+	SpringApplication.run(PubSubApplication.class, args);
+  }
+
+}
